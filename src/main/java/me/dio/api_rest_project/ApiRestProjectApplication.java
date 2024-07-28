0a8de95dc7f5a479bb727b9aa7733160aa396767
gusmrtns <gustavomartins9029@gmail.com>
2024-07-28 16:03:44 -0300

    Initial commit

---
+++ src/main/java/me/dio/api_rest_project/ApiRestProjectApplication.java
@@ -0,0 +1,13 @@
+package me.dio.api_rest_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestProjectApplication.class, args);
+	}
+
+}
